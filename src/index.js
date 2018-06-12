58c64367266000d36a6621c5abe1fd86b257460f
shreyas2047 <shreyass.hf95@gmail.com>
2018-06-11 23:08:23 -0700

    First Component called Hello

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={"React Ninja!!!!"} />, document.getElementById('root'));
 registerServiceWorker();
