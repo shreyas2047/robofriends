ce309b24b3f898a20de5a182c39d3dfbceee656c
shreyas2047 <shreyass.hf95@gmail.com>
2018-06-12 09:34:29 -0700

    Added the card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Hello greeting={"React Ninja!!!!"} />, document.getElementById('root'));
+ReactDOM.render(
+    <CardList robots={robots}/>
+    , document.getElementById('root'));
 registerServiceWorker();
